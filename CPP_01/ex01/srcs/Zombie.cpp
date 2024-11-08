87290bb298056c85044f202ed8d6da19bf526651
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-11-08 19:33:44 +0100

    progress

---
+++ CPP_01/ex01/srcs/Zombie.cpp
@@ -0,0 +1,22 @@
+#include "Zombie.hpp"
+
+void Zombie::announce()
+{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ...\n";
+}
+
+Zombie::Zombie()
+{
+	this->name = "default";
+}
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << this->name << " died" << std::endl;
+}
+
