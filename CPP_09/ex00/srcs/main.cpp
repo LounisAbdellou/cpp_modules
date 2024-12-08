cb9e4d8b45676d6e8dc4db2a086a09fe03addda3
LounisAbdellou <lounisabdellou.dev@gmail.com>
2024-12-08 22:37:32 +0100

    ok

--- CPP_09/ex00/srcs/main.cpp
+++ CPP_09/ex00/srcs/main.cpp
@@ -1,11 +1,26 @@
+#include <fstream>
 #include <iostream>
 
+static bool fileValidation(const std::string &filename) { return true; }
+
 int main(int ac, char **av) {
+  std::ifstream file;
+
   if (ac != 2) {
-    std::cout << "Error: wrong arguments" << std::endl;
+    std::cout << "Error: wrong arguments." << std::endl;
     return 1;
   }
 
-  std::cout << "test" << std::endl;
+  if (!fileValidation(av[1])) {
+    std::cout << "Error: file is not valid." << std::endl;
+    return 1;
+  }
+
+  file.open(av[1]);
+  if (!file.is_open()) {
+    std::cout << "Error: could not open file." << std::endl;
+    return 1;
+  }
+
   return 0;
 }
